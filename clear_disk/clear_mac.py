37223a1dae48388f5bb86039b961fc8a1ae501a6
xiewendan <stephenxjc@gmail.com>
2018-05-14 13:11:00 +0800

    add clear mac py

---
+++ clear_disk/clear_mac.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+# __author__ = stephenxjc@corp.netease.com
+# __date__ = 2018/5/14 下午12:49
+
+# desc: 清理磁盘空间
+
+
+class ClearMacMgr(object):
+    def __init__(self):
+        pass
+
+    def clear_all(self):
+        self.clear_xcode()
+
+    @staticmethod
+    def clear_xcode():
+        import os
+        import shutil
+        shutil.rmtree(r"/Users/stephenxjc/Library/Developer/Xcode/iOS DeviceSupport")
+        os.mkdir(r"/Users/stephenxjc/Library/Developer/Xcode/iOS DeviceSupport")
+
+
+def main():
+    clear_mac_mgr_obj = ClearMacMgr()
+    clear_mac_mgr_obj.clear_all()
+
+
+if __name__ == '__main__':
+    main()
