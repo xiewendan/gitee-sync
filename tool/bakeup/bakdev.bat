7598592b1f1cfd0dd2060bdb3a07cb393bb52e01
xiaobao <jinchunxie@126.com>
2019-05-13 21:27:55 +0800

    #0 清理windows指令

--- tool/bakeup/bakdev.bat
+++ tool/bakeup/bakdev.bat
@@ -4,14 +4,11 @@ echo %date%%time%
 
 echo 本地备份
 
-cwRsync\rsync -av --delete --stats /cygdrive/E/Documents/yitian/ /cygdrive/G/local/yitian/
-cwRsync\rsync -av --delete --stats /cygdrive/E/Documents/doc/ /cygdrive/G/local/doc/
-cwRsync\rsync -av --delete --stats /cygdrive/E/Documents/work/ /cygdrive/G/local/work/
+cwRsync\rsync -av --delete --stats /cygdrive/F/Documents/yitian/ /cygdrive/G/local/yitian/
+cwRsync\rsync -av --delete --stats /cygdrive/F/Documents/doc/ /cygdrive/G/local/doc/
+cwRsync\rsync -av --delete --stats /cygdrive/F/Documents/work/ /cygdrive/G/local/work/
 
-cwRsync\rsync -av --delete --stats /cygdrive/C/Users/xx.GAME/.vim /cygdrive/G/local/users/.vim
-cwRsync\rsync -av --delete --stats /cygdrive/C/Users/xx.GAME/Desktop /cygdrive/G/local/users/Desktop
-cwRsync\rsync -av --delete --stats /cygdrive/C/Users/xx.GAME/.viminfo /cygdrive/G/local/users/.viminfo
-cwRsync\rsync -av --delete --stats /cygdrive/C/Users/xx.GAME/_vimrc /cygdrive/G/local/users/_vimrc
+cwRsync\rsync -av --delete --stats /cygdrive/C/Users/xxx/Desktop /cygdrive/G/local/users/Desktop
 
 
 echo %date%%time%
