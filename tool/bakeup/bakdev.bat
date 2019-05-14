cc017af03c4fa68bc06015727319039b22ee5e84
xiaobao <jinchunxie@126.com>
2019-05-14 11:26:29 +0800

    #0 更新配置指令

--- tool/bakeup/bakdev.bat
+++ tool/bakeup/bakdev.bat
@@ -5,10 +5,9 @@ echo %date%%time%
 echo 本地备份
 
 cwRsync\rsync -av --delete --stats /cygdrive/F/Documents/yitian/ /cygdrive/G/local/yitian/
-cwRsync\rsync -av --delete --stats /cygdrive/F/Documents/doc/ /cygdrive/G/local/doc/
-cwRsync\rsync -av --delete --stats /cygdrive/F/Documents/work/ /cygdrive/G/local/work/
+cwRsync\rsync -av --delete --stats /cygdrive/F/Documents/config/ /cygdrive/G/local/config/
 
-cwRsync\rsync -av --delete --stats /cygdrive/C/Users/gzxiejinchun/Desktop /cygdrive/G/local/users/Desktop
+cwRsync\rsync -av --delete --stats /cygdrive/C/Users/xxx/Desktop /cygdrive/G/local/users/Desktop
 
 
 echo %date%%time%
