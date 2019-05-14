38f4a444b3c276451bfe3cfb626ed7ee5569a224
xiaobao <jinchunxie@126.com>
2019-05-14 16:55:52 +0800

    #0 更新配置指令

--- tool/bakeup/bakdev.bat
+++ tool/bakeup/bakdev.bat
@@ -4,10 +4,19 @@ echo %date%%time%
 
 echo 本地备份
 
-cwRsync\rsync -av --delete --stats /cygdrive/F/Documents/yitian/ /cygdrive/G/local/yitian/
-cwRsync\rsync -av --delete --stats /cygdrive/F/Documents/config/ /cygdrive/G/local/config/
+F:
 
-cwRsync\rsync -av --delete --stats /cygdrive/C/Users/xxx/Desktop /cygdrive/G/local/users/Desktop
+F:\Users\xxx\Documents\yitian
+
+cwRsync\rsync -av --delete --stats /cygdrive/F/Users/xxx/Documents/yitian/ /cygdrive/G/local/yitian/
+cwRsync\rsync -av --delete --stats /cygdrive/F/Users/xxx/Documents/config/ /cygdrive/G/local/config/
+
+cwRsync\rsync -av --delete --stats /cygdrive/D/"Program Files (x86)"/Netease/POPO/users/ /cygdrive/G/local/POPO/
+
+cwRsync\rsync -av --delete --stats /cygdrive/C/Users/xxx/Desktop/ /cygdrive/G/local/Desktop/
+
+cwRsync\rsync -av --delete --stats /cygdrive/C/Users/xxx/.PyCharmCE2019.1/config/keymaps/ /cygdrive/G/local/pycharmconfig/keymaps/
+cwRsync\rsync -av --delete --stats /cygdrive/C/Users/xxx/.PyCharmCE2019.1/config/templates/ /cygdrive/G/local/pycharmconfig/templates/
 
 
 echo %date%%time%
