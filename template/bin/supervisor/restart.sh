d3c8bcdf11ddb544686f9e7363c3a0c2f6b2fe84
xiaobao <jinchunxie@126.com>
2021-01-12 20:42:34 +0800

    #19 supervisord 1、新增重启脚本

---
+++ template/bin/supervisor/restart.sh
@@ -0,0 +1,17 @@
+echo before stop
+sh status.sh
+
+echo stop...
+sh stop.sh
+
+echo after stop
+sh status.sh
+
+echo before start
+sh status.sh
+
+echo start...
+sh start.sh
+
+echo after start
+sh status.sh
