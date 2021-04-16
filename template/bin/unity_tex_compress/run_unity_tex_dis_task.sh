896ea57186301aebdbb48d6938b39cd2c4cf74da
xiaobao <jinchunxie@126.com>
2021-04-16 14:09:08 +0800

    #38 分布式压缩贴图 1、跨平台方案：修复bug

--- template/bin/unity_tex_compress/run_unity_tex_dis_task.sh
+++ template/bin/unity_tex_compress/run_unity_tex_dis_task.sh
@@ -1,4 +1,4 @@
 cd ../..
 
-python3 main_frame/main.py -s unity_tex_dis_task 10.225.93.110 60010 /Users/g87/Documents/dis-compress/tools/template"/Applications/Unity/Hub/Editor/2019.4.0f1/Unity.app/Contents/Tools/PVRTexTool_orig -shh -flip y -f PVRTC1_2_RGB -q pvrtcbest -i Temp/aee4667d49fab44e7b1666ab4710dc60-feed.png -o Temp/aee4667d49fab44e7b1666ab4710dc60-res.pvr"
+python3 main_frame/main.py -s unity_tex_dis_task 10.225.93.110 60010 /Users/g87/Documents/dis-compress/tools/template "/Applications/Unity/Hub/Editor/2019.4.0f1/Unity.app/Contents/Tools/PVRTexTool -shh -flip y -f PVRTC1_2_RGB -q pvrtcbest -i Temp/aee4667d49fab44e7b1666ab4710dc60-feed.png -o Temp/aee4667d49fab44e7b1666ab4710dc60-res.pvr"
 cd bin/unity_tex_compress
