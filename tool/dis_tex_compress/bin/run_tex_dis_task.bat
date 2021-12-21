dedf42996441314ac1a2eb4652a9db0039045dde
xiaobao <jinchunxie@126.com>
2021-12-22 00:36:46 +0800

    #40 分布式压缩贴图k8s整合

--- tool/dis_tex_compress/bin/run_tex_dis_task.bat
+++ tool/dis_tex_compress/bin/run_tex_dis_task.bat
@@ -1,3 +1,3 @@
 cd ..
-python main_frame/main.py tex_dis_task "E:/project/xiewendan/tools/tool/dis_tex_compress/res/PVRTexTool -shh -flip y -f PVRTC1_2_RGB -q pvrtcbest -i E:/project/xiewendan/tools/tool/dis_tex_compress/data/tex/1.png -o E:/project/xiewendan/tools/tool/dis_tex_compress/data/tex/1.pvr"
+python main_frame/main.py tex_dis_task "E:/project/xiewendan/tools/tool/dis_tex_compress/res/PVRTexTool -shh -flip y -f PVRTC1_2_RGB -q pvrtcbest -i E:/project/xiewendan/tools/tool/dis_tex_compress/res/data/tex/1.png -o E:/project/xiewendan/tools/tool/dis_tex_compress/res/data/tex/1.pvr"
 cd bin
